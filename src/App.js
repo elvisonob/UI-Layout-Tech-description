24b283555865dacad269b6898d48a34211dc6daa
elvisonob <elvisonob@yahoo.com>
2024-10-19 20:00:22 +0100

    Layout getting in sync

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ const App = () => {
   return (
     <div className="container">
       <div className="header">
-        <div className="companyLogo1">Company Logo1</div>
-        <div className="companyLogo2">Company Logo2</div>
-        <div className="search">Search</div>
-        <div className="signIn">signIn</div>
+        <div className="companyLogo1">
+          <img alt="logo1" src={hearts} width="100%" height="70%" />
+        </div>
+        <div className="companyLogo2">
+          <img alt="logo2" src={institute} width="80%" height="80%" />
+        </div>
+
+        <div className="search">
+          <div className="topSearch">
+            <div className="search">Search</div>
+            <div className="icon">
+              <img src={search} alt="search-item" width="60%" height="50%" />
+            </div>
+          </div>
+          <div className="bottomSearch"></div>
+        </div>
+        <div className="signIn">Sign In</div>
         <div className="basket">basket</div>
       </div>
       <div className="menu">
