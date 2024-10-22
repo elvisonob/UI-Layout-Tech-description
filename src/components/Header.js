d5a909be9fd105fe9065b7512ef9c95f69f6dfa5
elvisonob <elvisonob@yahoo.com>
2024-10-22 18:45:45 +0100

    anchor tag added

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,12 +11,19 @@ const Header = () => {
         <img alt="logo1" src={hearts} width="100%" height="70%" />
       </div>
       <div className="companyLogo2">
-        <img alt="logo2" src={institute} width="80%" height="80%" />
+        <a
+          href="https://www.energyinst.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img alt="logo2" src={institute} width="80%" height="80%" />
+        </a>
       </div>
-
       <div className="search">
         <div className="topSearch">
-          <div className="search">Search</div>
+          <div text="" className="search">
+            Search
+          </div>
           <div className="icon">
             <img src={search} alt="search-item" width="60%" height="50%" />
           </div>
